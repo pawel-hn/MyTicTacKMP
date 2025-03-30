8170f826808bc4b3e804faaeb9ba337ac0367909
Pawel <pawel.hnacik@applandeo.pl>
2025-03-30 14:33:54 +0200

    Remove tictac back handler

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
 
@@ -12,39 +11,24 @@ plugins {
 
 kotlin {
     cocoapods {
-        // Required properties
-        // Specify the required Pod version here
-        // Otherwise, the Gradle project version is used
         version = "1.0"
         summary = "Kotlin/Native module"
         homepage = "nope"
 
-        // Optional properties
-        // Configure the Pod name here instead of changing the Gradle project name
         name = "MyCocoaPod"
 
         framework {
-            // Required properties
-            // Framework name configuration. Use this property instead of deprecated 'frameworkName'
             baseName = "MyFramework"
             binaryOption("bundleId", "org.mytictackmp.app")
-
-            // Optional properties
-            // Specify the framework linking type. It's dynamic by default.
             isStatic = false
-            // Dependency export
-            // Uncomment and specify another project module if you have one:
-            // export(project(":<your other KMP module>"))
-            transitiveExport = false // This is default.
+            transitiveExport = false
         }
 
-        // Maps custom Xcode configuration to NativeBuildType
         xcodeConfigurationToNativeBuildType["CUSTOM_DEBUG"] = NativeBuildType.DEBUG
         xcodeConfigurationToNativeBuildType["CUSTOM_RELEASE"] = NativeBuildType.RELEASE
     }
 
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
--- composeApp/src/androidMain/kotlin/org/mytictackmp/app/ui/components/TicTacBackHandler.android.kt
+++
@@ -1,9 +0,0 @@
-package org.mytictackmp.app.ui.components
-
-import androidx.activity.compose.BackHandler
-import androidx.compose.runtime.Composable
-
-@Composable
-actual fun TicTacBackHandler(onBack: () -> Unit) {
-    BackHandler { onBack() }
-}
